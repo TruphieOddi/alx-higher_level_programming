4f56702f07d5b8c50ef2f89dbf0ae1e957180f54
TruphieOddi <trulietruphie@gmail.com>
2023-11-21 04:31:08 -0800

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""a class Square that defines a square by: (based on 2-square.py)"""
+
+
+class Square:
+    """still defining the same square class"""
+    def __init__(self, size=0):
+        """Creates new instances of sq"""
+        self.__size = size
+
+    def area(self):
+        """Calculates area of said square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Returns property size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method used"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """prints the said square finally"""
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * (self.__size))
