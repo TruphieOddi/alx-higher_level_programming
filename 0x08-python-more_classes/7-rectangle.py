28f5516b45cab42a699fa370c9a7df5b17d3a239
TruphieOddi <trulietruphie@gmail.com>
2023-11-27 11:30:54 -0800

    updates

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -58,8 +58,8 @@ class Rectangle:
         """Prints the rectangle in #form"""
         if self.__width == 0 or self.__height == 0:
             return ("")
-
-        return "\n".join
+        if i != self.__height - 1:
+            return "\n".join
     ([str(self.print_symbol) * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
