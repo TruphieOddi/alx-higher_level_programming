e7c27444a9b85b3181c79c78d7d84f8d54b4182c
TruphieOddi <trulietruphie@gmail.com>
2023-11-29 03:33:12 -0800

    task 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a function that divides all elements of a matrix."""
+
+
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix.
+
+    Args:
+        matrix (list of lists): The matrix of integers or floats.
+        div (int or float): The number to divide all elements of the matrix.
+    """
+
