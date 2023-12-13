2937733ab9e53604ca36c056eec7f5cdfcb86483
TruphieOddi <trulietruphie@gmail.com>
2023-12-13 00:53:33 -0800

    task 0

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module base that defines a base model class"""
+
+
+import jason
+import os
+import csv
+
+class Base:
+    """This is the base for the incoming tasks with private class attribute:
+    __nb_objects"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialization of a Base instance."""
+        
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
